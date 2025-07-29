95489ee8f5a02ae48d1d2b2bc43acdd1b57f4ffb
Gilbert <27494820+Gilbert-S@users.noreply.github.com>
2025-07-29 18:21:13 +0200

    Add custom PowerShell aliases and update terminal settings
    * Introduced PowerShell aliases for grep and awk using WSL, and added git branch cleanup aliases.
    * Updated Windows Terminal settings: reorganized command IDs, added new key bindings, and included a Git Bash profile.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -7,3 +7,13 @@ function Set-WindowTitle {
   $host.UI.RawUI.WindowTitle = [string]::Join(" ", $args)
 }
 Set-Alias -name "title" -value Set-WindowTitle
+
+# Set-Alias -name "grep" -value "Select-String" | ft
+function f_grep {$input | wsl -e grep @args}
+Set-Alias -name "grep" -value f_grep
+
+function f_awk {$input | wsl -e awk @args}
+Set-Alias -name "awk" -value f_awk
+
+git config --global alias.gone "!git branch -vv | awk '/: gone]/{print `$1}'"
+git config --global alias.gonedelete "! git branch -d `$(git gone)"
